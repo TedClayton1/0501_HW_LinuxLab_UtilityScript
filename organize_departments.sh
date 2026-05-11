c3a2a0999534edc9d3e577e8e0c201ffc5b87119
Ted <ted_clayton@hotmail.com>
2026-05-11 19:26:56 -0400

    Completed print messages and forwarded them to base directory and and appropriate file path(s).

--- organize_departments.sh
+++ organize_departments.sh
@@ -26,3 +26,10 @@ touch "$BASE_DIR/hr/employee_promotions.txt"
 touch "$BASE_DIR/sales/sales_reports.txt"
 touch "$BASE_DIR/finance/financial_statements.txt"
 touch "$BASE_DIR/it/technical_documents.txt"
+
+# Print messages to terminal and write content into file(s)
+echo "HR employee promotions file created for HR department." > "$BASE_DIR/hr/employee_promotions.txt"
+echo "Sales reports file also created for the sales department." > "$BASE_DIR/sales/sales_reports.txt"
+echo "Financial statements file created for the finance dept." > "$BASE_DIR/finance/financial_statements.txt"
+echo "Technical documents file created for the IT department." > "$BASE_DIR/it/technical_documents.txt"
+
